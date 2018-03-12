331bf651f82d15441fe59749808bd3f52156fba3
龚少瑜 <gongshaoyu@xindong.com>
2018-03-12 18:07:32 +0800

    基础功能

---
+++ example/test.php
@@ -0,0 +1,31 @@
+<?php
+
+require "../src/ApiFactory.php";
+require "../src/Curl.php";
+require "../src/CurlResponse.php";
+require "../src/HttpApi.php";
+
+try
+{
+    $test = \CdkeyClient\ApiFactory::getInstance('lqwy', 'console', 'http://127.0.0.1:9899/api/', 'lqwy');
+    $doRequest = $test->createGiftbag([
+        'name'                => '100cdkey',
+        'items'               => 'test100',
+        'type'                => 12,
+        'produce_count'       => 100,
+        'create_admin_id'     => 1,
+        'start_time'          => 1517155200,
+        'end_time'            => 1543593600,
+        'receive_only_once'   => 1,
+        'band_only_once'      => 1,
+        'use_only_once'       => 1,
+        'code_auto_produce'   => 1,
+        'receive_produce'     => 1,
+        'receive_random_gift' => 1,
+        'status'              => 1,
+    ]);
+
+    echo $doRequest;
+} catch (Exception $e) {
+    echo json_encode(['status' => false, 'msg' => 'inner error']);
+}
---
+++ src/HttpApi.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gongshaoyu
+ * Date: 2018/3/8
+ * Time: 下午12:22
+ */
+namespace CdkeyClient;
+
+class HttpApi extends Curl
+{
+    /**
+     * 接口地址
+     * @var string
+     */
+    public $host;
+
+    /**
+     * 接口token
+     * @var string
+     */
+    public $token;
+
+    /**
+     * HttpApi constructor.
+     * @param $host
+     * @param $port
+     * @param $token
+     */
+    public function __construct($host, $token)
+    {
+        parent::__construct();
+        $this->host  = $host;
+        $this->token = $token;
+    }
+
+    /**
+     * 获取url地址
+     * @param string $action
+     * @param array $data
+     * @return string
+     */
+    public function getUrl($action, $data = [])
+    {
+        $time = time();
+        $data['time'] = $time;
+        $data['sign'] = $this->doSign($time, $this->token);
+        return $this->host . '/' . $action . http_build_url($data);
+    }
+
+    /**
+     * 计算接口sign
+     * @param int $time
+     * @param string $token
+     * @return string
+     */
+    public function doSign($time, $token)
+    {
+        return md5($time . $token);
+    }
+
+    /**
+     * 创建礼包
+     * @param $data
+     * @return bool|CurlResponse
+     */
+    public function createGiftbag($data)
+    {
+        $url = $this->getUrl("giftbag/add");
+        return $this->post($url, $data);
+    }
+
+    /**
+     * 更新礼包信息
+     * @param $data
+     * @return bool|CurlResponse
+     */
+    public function updateGiftbag($data)
+    {
+        $url = $this->getUrl("giftbag/edit");
+        return $this->post($url, $data);
+    }
+
+    /**
+     * 查询礼包列表
+     * @param array $data
+     * @return CurlResponse
+     */
+    public function giftbagList($data = [])
+    {
+        $url = $this->getUrl('giftbag/list', $data);
+        return $this->get($url, $data);
+    }
+
+    /**
+     * 礼包详情
+     * @param $data
+     * @return CurlResponse
+     */
+    public function giftbagDetil($data)
+    {
+        $url = $this->getUrl('giftbag/detail', $data);
+        return $this->get($url, $data);
+    }
+
+    /**
+     * 礼包追加礼券
+     * @param $data
+     * @return CurlResponse
+     */
+    public function increaseGiftbag($data)
+    {
+        $url = $this->getUrl('giftbag/increase', $data);
+        return $this->post($url, $data);
+    }
+
+    /**
+     * 领取礼券
+     * @param $data
+     * @return bool|CurlResponse
+     */
+    public function receiveCdkey($data)
+    {
+        $url = $this->getUrl('cdkey/receive', $data);
+        return $this->post($url, $data);
+    }
+
+    /**
+     * 绑定礼券
+     * @param $data
+     * @return bool|CurlResponse
+     */
+    public function bandCdkey($data)
+    {
+        $url = $this->getUrl('cdkey/band', $data);
+        return $this->post($url, $data);
+    }
+
+    /**
+     * 使用礼券
+     * @param $data
+     * @return bool|CurlResponse
+     */
+    public function useCdkey($data)
+    {
+        $url = $this->getUrl('cdkey/use', $data);
+        return $this->post($url, $data);
+    }
+
+    /**
+     * 礼券详情
+     * @param $data
+     * @return CurlResponse
+     */
+    public function cdkeyDetil($data)
+    {
+        $url = $this->getUrl('cdkey/detail', $data);
+        return $this->get($url, $data);
+    }
+}
