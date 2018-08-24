74fdbea37d75c3e7d62083f95f492e7b349a4b80
龚少瑜 <gongshaoyu@xindong.com>
2018-08-24 18:59:07 +0800

    删除无用依赖包

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,24 +47,6 @@ class ComposerAutoloaderInitbb7379ca88ec2905d7a92e903cfac834
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInitbb7379ca88ec2905d7a92e903cfac834::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequirebb7379ca88ec2905d7a92e903cfac834($fileIdentifier, $file);
-        }
-
         return $loader;
-    }
-}
-
-function composerRequirebb7379ca88ec2905d7a92e903cfac834($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
